ceff1cbb14d45072d56daf7b80325a7bd6efc30d
brunograssano <grassanobruno@gmail.com>
2021-10-26 15:17:26 -0300

    feat: Added fibonacci exercise (1)

--- src/exercise1.ts
+++ src/exercise1.ts
@@ -1,3 +1,14 @@
+
+const fib_rec = (n:number,prev_a:number,prev_b:number, acum:number): number => {
+  if ( n == 0) {
+    return acum;
+  }
+  return fib_rec(n-1,prev_b,prev_a + prev_b, prev_a + prev_b);
+};
+
 export const fibonacci = (n: number): number => {
-  throw new Error('Not implemented');
+  if (n == 0){
+    return 0;
+  }
+  return fib_rec(n-1,0,1,1);
 };
