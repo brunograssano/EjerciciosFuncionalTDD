20a3b1730255c8b27a4ee395952d0d9e92d635ff
brunograssano <grassanobruno@gmail.com>
2021-10-27 10:16:56 -0300

    ref: Removed an argument from fib_rec

--- src/exercise1.ts
+++ src/exercise1.ts
@@ -1,14 +1,14 @@
 
-const fib_rec = (n:number,prev_a:number,prev_b:number, acum:number): number => {
-  if ( n == 0) {
-    return acum;
+const fib_rec = (n:number,previous:number,accumulated:number): number => {
+  if ( n <= 0) {
+    return accumulated;
   }
-  return fib_rec(n-1,prev_b,prev_a + prev_b, prev_a + prev_b);
+  return fib_rec(n-1,accumulated,previous + accumulated);
 };
 
 export const fibonacci = (n: number): number => {
-  if (n == 0){
+  if (n <= 0){
     return 0;
   }
-  return fib_rec(n-1,0,1,1);
+  return fib_rec(n-1,0,1);
 };
