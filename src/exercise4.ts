2baf434939d722bebcad68fbcbeb6e2e722e26f3
brunograssano <grassanobruno@gmail.com>
2021-11-03 10:28:41 -0300

    ref: Made more explicit sumDuplicateWords
    -m "Closes #1"

--- src/exercise4.ts
+++ src/exercise4.ts
@@ -1,18 +1,10 @@
 
-const countWords = (value: string) :  Record<string, number> => {
-  let word: Record<string,number> = {}
-  word[value] = 1
-  return word;
-};
-
-const sumDuplicateWords = (countedWords: Record<string, number>, currentWord: Record<string, number>): Record<string, number> => {
-  let keys = Object.keys(currentWord)
-  if(countedWords[keys[0]]){
-    let count = countedWords[keys[0]];
-    countedWords[keys[0]] = count + 1;
+const sumDuplicateWords = (countedWords: Record<string, number>, currentWord: string): Record<string, number> => {
+  if(currentWord in countedWords){
+    countedWords[currentWord] += 1;
   }
   else{
-    countedWords[keys[0]] = 1;
+    countedWords[currentWord] = 1;
   }
   return countedWords;
 };
@@ -26,5 +18,5 @@ export const wordCount = (sequence: string): Record<string, number> => {
     return {};
   }
   let words = sequence.toLowerCase().split(" ");
-  return  words.filter(isValidString).map(countWords).reduce(sumDuplicateWords,{});
+  return  words.filter(isValidString).reduce(sumDuplicateWords,{});
 };
